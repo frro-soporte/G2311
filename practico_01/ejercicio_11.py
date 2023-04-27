fc67cb1b4367313b8a4842cc0a50494bd0395c6f
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-04-26 21:40:43 -0300

    Realizar ejercicio 11

--- practico_01/ejercicio_11.py
+++ practico_01/ejercicio_11.py
@@ -10,9 +10,14 @@ def suma_cubo_pares_for(numeros: Iterable[int]) -> int:
     Restricción: Utilizar dos bucles for, uno para elevar al cubo y otro para
     separar los pares.
     """
-    pass # Completar
-
-
+    lista1 = list()
+    lista2 = list()
+    for num in numeros:
+        lista1.append(num ** 3)
+    for num in lista1:
+        if num % 2 == 0:
+            lista2.append(num)
+    return sum(lista2)
 # NO MODIFICAR - INICIO
 assert suma_cubo_pares_for([1, 2, 3, 4, 5, 6]) == 288
 # NO MODIFICAR - FIN
@@ -28,7 +33,7 @@ def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
     Referencia: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-    pass # Completar
+    return sum([x**3 for x in numeros if x % 2 == 0])
 
 
 # NO MODIFICAR - INICIO
@@ -44,7 +49,7 @@ def suma_cubo_pares_sum_gen(numeros: Iterable[int]) -> int:
     y la función sum.
     Referencia: https://docs.python.org/3/reference/expressions.html#generator-expressions
     """
-    pass # Completar
+    return sum((x**3 for x in numeros if x % 2 == 0))
 
 
 # NO MODIFICAR - INICIO
@@ -64,25 +69,25 @@ numeros = [1, 2, 3, 4, 5, 6]
 
 # Escribir una función lambda que eleve los elementos al cubo
 
-numeros_al_cubo = # Completar
+numeros_al_cubo = list(map(lambda x: x**3, numeros))
 
 
 # Escribir una función lambda que permita filtrar todos los elementos pares
 
-numeros_al_cubo_pares = # Completar
+numeros_al_cubo_pares = list(filter(lambda x : x % 2 == 0 , numeros_al_cubo))
 
 
 # Escribir una función Lambda que sume todos los elementos
 
 from functools import reduce
 
-suma_numeros_al_cubo_pares = # Completar
+suma_numeros_al_cubo_pares = reduce(lambda x, y: x+y, numeros_al_cubo_pares)
 
 
 # Escribir una función Lambda que permita ordenar los elementos de la numeros
 # en base a si son pares o impares
 
-numeros_ordenada = # Completar
+numeros_ordenada = sorted(numeros, key =lambda x : x % 2 == 0)
 
 # NO MODIFICAR - INICIO
 assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
