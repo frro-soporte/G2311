56a58e52bc394cee343f34d5f5b40afd3970b957
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-04-28 16:41:59 -0300

    Realizar ejercicio 14

--- practico_01/ejercicio_14.py
+++ practico_01/ejercicio_14.py
@@ -15,12 +15,17 @@ from typing import Callable, Iterable
 def apply(lista: Iterable[int], func: Callable[[int], bool]) -> Iterable[bool]:
     """Toma una lista y una función que toma un parámetro y devuelve una lista
     con la función aplicada a todos los elementos."""
-    pass # Completar
+    lista2 = []
+    for elem in lista:
+        lista2.append(func(elem))
+    return lista2
 
 
 # NO MODIFICAR - INICIO
 def esta_entre_valores(x: int, min_: float, max_: float) -> bool:
     return min_ < x < max_
+
+
 # NO MODIFICAR - FIN
 
 
@@ -34,7 +39,7 @@ Referencia: https://docs.python.org/3/library/functools.html#functools.partial
 lista = [3, 4, 5, 6, 7, 8]
 min_ = 4
 max_ = 7
-nueva_funcion = # Completar
+nueva_funcion = partial(esta_entre_valores, min_=min_, max_=max_)
 
 # NO MODIFICAR - INICIO
 lista = [3, 4, 5, 6, 7, 8]
