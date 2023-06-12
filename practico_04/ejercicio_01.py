16a32035f6bfa831e7d715e7268ee639fd1adf4d
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-06-12 00:16:54 -0300

    Update ejercicio_01.py

--- practico_04/ejercicio_01.py
+++ practico_04/ejercicio_01.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 
+
 def crear_tabla():
     """Implementar la funcion crear_tabla, que cree una tabla Persona con:
         - IdPersona: Int() (autoincremental)
@@ -10,13 +11,34 @@ def crear_tabla():
         - DNI: Int()
         - Altura: Int()
     """
-    pass # Completar
+    conn = sqlite3.connect("databaseEj4.db")
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Persona(
+        idPersona integer PRIMARY KEY AUTOINCREMENT, 
+        nombre varchar(30), 
+        fechaNacimiento datetime, 
+        dni integer,
+        altura integer
+        )
+    """)
+    conn.commit()
+    conn.close()
 
 
 def borrar_tabla():
     """Implementar la funcion borrar_tabla, que borra la tabla creada 
     anteriormente."""
-    pass # Completar
+
+    conn = sqlite3.connect("databaseEj4.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+        DROP TABLE Persona
+    """)
+
+    conn.commit()
+    conn.close()
 
 
 # NO MODIFICAR - INICIO
@@ -25,5 +47,6 @@ def reset_tabla(func):
         crear_tabla()
         func()
         borrar_tabla()
+
     return func_wrapper
 # NO MODIFICAR - FIN
