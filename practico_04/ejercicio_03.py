c4eb8bcc863560c98cb6a32a65e8b9e59dcb8866
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-06-12 00:17:35 -0300

    Update ejercicio_03.py

--- practico_04/ejercicio_03.py
+++ practico_04/ejercicio_03.py
@@ -1,6 +1,7 @@
 """Base de Datos SQL - Baja"""
 
 import datetime
+import sqlite3
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
@@ -10,7 +11,14 @@ def borrar_persona(id_persona):
     """Implementar la funcion borrar_persona, que elimina un registro en la 
     tabla Persona. Devuelve un booleano en base a si encontro el registro y lo 
     borro o no."""
-    pass # Completar
+    conn = sqlite3.connect("databaseEj4.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM Persona WHERE idPersona = ?", (id_persona,))
+    filas_borradas = cur.rowcount
+    conn.commit()
+    cur.close()
+    conn.close()
+    return filas_borradas > 0
 
 # NO MODIFICAR - INICIO
 @reset_tabla
